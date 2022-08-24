9b99c6b6c627b5deca3b0bf63a2ad2fbfe9ed3b7
Igor <lomakinigor96@gmail.com>
2022-08-24 23:07:30 +0300

    Add greeting

--- src/main/java/com/upskill/rentcars/RentCarsApplication.java
+++ src/main/java/com/upskill/rentcars/RentCarsApplication.java
@@ -2,12 +2,19 @@ package com.upskill.rentcars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RentCarsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RentCarsApplication.class, args);
     }
 
+    @GetMapping
+    public String greeting(){
+        return "Welcome to Rent Cars!";
+    }
 }
