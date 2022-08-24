bc469fc4aa3f27bf9a19667e9054791f01320e8d
Igor <lomakinigor96@gmail.com>
2022-08-24 22:50:31 +0300

    Initial commit

---
+++ src/main/java/com/upskill/rentcars/RentCarsApplication.java
@@ -0,0 +1,13 @@
+package com.upskill.rentcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarsApplication.class, args);
+    }
+
+}
